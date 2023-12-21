f6d9fc8732504a3f9180bffccd68c1e8992921a5
hwakkub <152359933+hwakkub@users.noreply.github.com>
2023-12-21 11:02:45 +0700

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by jirakit on 21/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
